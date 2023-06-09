09627b1b4b2a68c08fef63f12e2f544b7cccf085
Jorge Rebollo <jorgerebolloj@gmail.com>
2023-06-09 16:23:43 -0600

    Base project

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by XIPEHK on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
